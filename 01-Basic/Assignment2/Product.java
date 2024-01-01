ab7703ce572712255e74c8db93cf1de5316bedf9
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-01-01 18:22:36 +0600

    Work in Basic part

---
+++ 01-Basic/Assignment2/Product.java
@@ -0,0 +1,28 @@
+/*
+ * Assignment-2 (Variable and Data type)
+ * step 1: create a class called Product
+ * step 2: create a main method
+ * step 3: declare variables: id, title, price, description, category
+ * step 4: assign the following data in main method
+ * 101,iphone15,1895 euros,perfect product with best image quality, phone,
+* step 5: print the data
+ */
+
+
+package Assignment2;
+
+public class Product {
+    public static void main(String[] args) {
+        int id = 101;
+        String title = "iphone15";
+        float price = 1895;
+        String description = "perfect product with best image quality";
+        String category = "phone";
+
+        System.err.println(id);
+        System.err.println(title);
+        System.err.println(price+" euros");
+        System.err.println(description);
+        System.err.println(category);
+    }
+}
---
+++ 01-Basic/Comments/Comments.java
@@ -0,0 +1,14 @@
+package Comments;
+
+public class Comments {
+    public static void main(String[] args) {
+        
+        // This is single line comment
+
+        /*
+         This is multiline comment
+         This is multiline comment
+         This is multiline comment
+         */
+    }
+}
